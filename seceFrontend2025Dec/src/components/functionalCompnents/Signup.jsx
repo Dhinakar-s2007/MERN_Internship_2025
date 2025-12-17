c2953332e43aa4808c7d1df0c7acbf27f2861866
Prasanth K S <ksprasanthksp10@gmail.com>
2025-12-17 10:09:49 +0530

    Modified Signup Page

--- seceFrontend2025Dec/src/components/functionalCompnents/Signup.jsx
+++ seceFrontend2025Dec/src/components/functionalCompnents/Signup.jsx
@@ -1,24 +1,65 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignup = async (event) => {
+    event.preventDefault();
+
+    try {
+      const req = await axios.post("http://localhost:8001/signup", {
+        email,
+        username,
+        password,
+      });
+
+      const { message, isSignup } = req.data;
+
+      if (isSignup) {
+        alert(message);
+        navigate("/login");
+      }
+    } catch (e) {
+      alert("Login Failed", e);
+    }
+  };
+
   return (
     <div>
       <h2>Signup Page</h2>
-      <form>
+      <form onSubmit={handleSignup}>
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <br />
         <br />
         <label>
           Username:
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
         <br />
         <br />
         <label>
           Password:
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <br />
         <br />
